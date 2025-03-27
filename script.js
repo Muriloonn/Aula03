b8a597dc895569feff2727487373affaae51dbdb
Muriloonn <murilo.mpina@gmail.com>
2025-03-27 09:51:42 -0300

    Tirando o parseInt do script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let nome = prompt('Qual o seu nome?');
-let idade = prompt(parseInt('Quantos anos você tem?'));
+let idade = prompt('Quantos anos você tem?');
 console.log(nome + ' ' + idade);
 alert(`Você chama ${nome} e possui ${idade} anos!`);
