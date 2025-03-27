90524b646aa4e6445f974d9adc19e07afb284a3e
Muriloonn <murilo.mpina@gmail.com>
2025-03-27 09:31:25 -0300

    Primeiro commit do Desafio 1

---
+++ script.js
@@ -0,0 +1,4 @@
+let nome = prompt('Qual o seu nome?');
+let idade = prompt(parseInt('Quantos anos você tem?'));
+console.log(nome + ' ' + idade);
+alert(`Você chama ${nome} e possui ${idade} anos!`);
